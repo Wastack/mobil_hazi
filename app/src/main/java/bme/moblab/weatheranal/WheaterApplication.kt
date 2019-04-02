2d9dbc4bcaf2e4e5d3f64c0ffcba3dd46348f463
Tamas Bunth <tamas.bunth@collabora.co.uk>
2019-04-02 15:02:54 +0200

    Override Application

---
+++ app/src/main/java/bme/moblab/weatheranal/WheaterApplication.kt
@@ -0,0 +1,11 @@
+package bme.moblab.weatheranal
+
+import android.app.Application
+
+class WheaterApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+}
